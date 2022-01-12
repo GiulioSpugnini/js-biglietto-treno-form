4fdf1b0f718159b01587383cf3afc7ea4b49d6b3
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-12 15:26:02 +0100

    discount control inserted inside button function

--- js/script.js
+++ js/script.js
@@ -5,38 +5,38 @@ const travelElement = document.getElementById("travel");
 
 const ageElement = document.getElementById("age");
 
-// #Calcolare il prezzo per chilometro
-const ticketPrice = 0.21 * travelElement;
-let userDiscount;
-let finalPrice;
-// 3b. Applicare sconto 20% per i minorenni
-if (ageElement.value === 'Minorenne') {
-    userDiscount = (ticketPrice * 0.20).toFixed(2);
-
-    finalPrice = (ticketPrice - userDiscount).toFixed(2);
-
-    console.log('Il prezzo dopo aver applicato lo sconto è: ', finalPrice);
-
-
-} else if (ageElement.value === 'Maggiorenne') {
-    userDiscount = (ticketPrice * 0.40).toFixed(2);
-    console.log('Lo sconto per il passeggero è stato di: ', userDiscount);
-    finalPrice = (ticketPrice - userDiscount).toFixed(2);
-
-    console.log('Il prezzo dopo aver applicato lo sconto è: ', finalPrice);
-
-} else {
-    ageElement.value === 'Altro';
-    userDiscount = 0;
-    finalPrice = ticketPrice;
-    console.log('Lo sconto per il passeggero è stato di: 0');
-    console.log('Il prezzo del biglietto è: ', ticketPrice);
-}
 
 const generate = document.getElementById("generate");
 const cancel = document.getElementById("cancel");
 
 generate.addEventListener('click', function() {
+    // #Calcolare il prezzo per chilometro
+    const ticketPrice = 0.21 * travelElement.value;
+    let userDiscount;
+    let finalPrice;
+    // 3b. Applicare sconto 20% per i minorenni
+    if (ageElement.value === 'Minorenne') {
+        userDiscount = (ticketPrice * 0.20).toFixed(2);
+
+        finalPrice = (ticketPrice - userDiscount).toFixed(2);
+
+        console.log('Il prezzo dopo aver applicato lo sconto è: ', finalPrice);
+
+
+    } else if (ageElement.value === 'Maggiorenne') {
+        userDiscount = (ticketPrice * 0.40).toFixed(2);
+        console.log('Lo sconto per il passeggero è stato di: ', userDiscount);
+        finalPrice = (ticketPrice - userDiscount).toFixed(2);
+
+        console.log('Il prezzo dopo aver applicato lo sconto è: ', finalPrice);
+
+    } else {
+        ageElement.value === 'Altro';
+        userDiscount = 0;
+        finalPrice = ticketPrice;
+        console.log('Lo sconto per il passeggero è stato di: 0');
+        console.log('Il prezzo del biglietto è: ', ticketPrice);
+    }
     const inputName = nameElement.value;
     const inputTravel = travelElement.value;
     const inuputAge = ageElement.value;
