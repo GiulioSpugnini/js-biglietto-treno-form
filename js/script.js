497eb43d5dbb6455fa6b839920ddae468db6e979
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-12 14:09:28 +0100

    added constants

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log('JS ok!');
+const nameElement = document.getElementById("name");
+console.log(nameElement);
+const travelElement = document.getElementById("travel");
+console.log(travelElement);
+const ageElement = document.getElementById("age");
+console.log(ageElement);
