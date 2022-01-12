9f5d0e9afcd2d3dbc1966db0ee3e5802cea0f556
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-12 17:39:45 +0100

    added alert in js if the user write incorrects values

--- js/script.js
+++ js/script.js
@@ -16,50 +16,55 @@ const codeCP = document.getElementById("codeCP");
 const price = document.getElementById("price");
 
 generate.addEventListener('click', function() {
-    // #Calcolare il prezzo per chilometro
-    const ticketPrice = 0.21 * travelElement.value;
-    let userDiscount;
-    let discountType;
-    let finalPrice;
-    // 3b. Applicare sconto 20% per i minorenni
-    if (ageElement.value === 'Minorenne') {
-        userDiscount = (ticketPrice * 0.20).toFixed(2);
-        discountType = 'Biglietto Sconto Minorenne';
-        finalPrice = (ticketPrice - userDiscount).toFixed(2);
-
-        console.log('Il prezzo dopo aver applicato lo sconto è: ', finalPrice);
-
-
-    } else if (ageElement.value === 'Maggiorenne') {
-        userDiscount = (ticketPrice * 0.40).toFixed(2);
-        discountType = 'Biglietto Sconto Over65';
-
-        console.log('Lo sconto per il passeggero è stato di: ', userDiscount);
-        finalPrice = (ticketPrice - userDiscount).toFixed(2);
-
-        console.log('Il prezzo dopo aver applicato lo sconto è: ', finalPrice);
-
+    //!VALIDATE
+    if (!isNaN(nameElement.value) || travelElement.value < 0) {
+        alert("DEVI COMPILARE CORRETTAMENTE GLI SPAZI");
+        window.location.reload();
     } else {
-        ageElement.value === 'Altro';
-        userDiscount = 0;
-        discountType = 'Biglietto Standard';
+        // #Calcolare il prezzo per chilometro
+        const ticketPrice = 0.21 * travelElement.value;
+        let userDiscount;
+        let discountType;
+        let finalPrice;
+        // 3b. Applicare sconto 20% per i minorenni
+        if (ageElement.value === 'Minorenne') {
+            userDiscount = (ticketPrice * 0.20).toFixed(2);
+            discountType = 'Biglietto Sconto Minorenne';
+            finalPrice = (ticketPrice - userDiscount).toFixed(2);
 
-        finalPrice = ticketPrice;
-        console.log('Lo sconto per il passeggero è stato di: 0');
-        console.log('Il prezzo del biglietto è: ', ticketPrice);
+            console.log('Il prezzo dopo aver applicato lo sconto è: ', finalPrice);
+
+
+        } else if (ageElement.value === 'Maggiorenne') {
+            userDiscount = (ticketPrice * 0.40).toFixed(2);
+            discountType = 'Biglietto Sconto Over65';
+
+            console.log('Lo sconto per il passeggero è stato di: ', userDiscount);
+            finalPrice = (ticketPrice - userDiscount).toFixed(2);
+
+            console.log('Il prezzo dopo aver applicato lo sconto è: ', finalPrice);
+
+        } else {
+            ageElement.value === 'Altro';
+            userDiscount = 0;
+            discountType = 'Biglietto Standard';
+
+            finalPrice = ticketPrice;
+            console.log('Lo sconto per il passeggero è stato di: 0');
+            console.log('Il prezzo del biglietto è: ', ticketPrice);
+        }
+        let inputName = nameElement.value;
+        let inputTravel = travelElement.value;
+        const inuputAge = ageElement.value;
+        console.log(inputName, inputTravel, inuputAge);
+        //#RESULTS
+        nameResult.innerHTML = `<h6 class="fw-bolder">${nameElement.value}</h6>`;
+        discount.innerHTML = `<h6>${discountType}</h6>`;
+        cabin.innerHTML = `<h6>${Math.floor(Math.random() * 12) + 1}</h6>`;
+        codeCP.innerHTML = `<h6>${Math.floor(Math.random() * 10000) + 1}</h6>`;
+        price.innerHTML = `<h6>${finalPrice} €</h6>`;
+
     }
-    let inputName = nameElement.value;
-    let inputTravel = travelElement.value;
-    const inuputAge = ageElement.value;
-    console.log(inputName, inputTravel, inuputAge);
-    //#RESULTS
-    nameResult.innerHTML = `<h6 class="fw-bolder">${nameElement.value}</h6>`;
-    discount.innerHTML = `<h6>${discountType}</h6>`;
-    cabin.innerHTML = `<h6>${Math.floor(Math.random() * 12) + 1}</h6>`;
-    codeCP.innerHTML = `<h6>${Math.floor(Math.random() * 10000) + 1}</h6>`;
-    price.innerHTML = `<h6>${finalPrice} €</h6>`;
-
-
 })
 
 cancel.addEventListener('click', function() {
