ae40d741bdde75e014486d45ebed817ad5d73d6f
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-01-12 15:14:49 +0100

    added control of age and price of ticket with discount or not

--- js/script.js
+++ js/script.js
@@ -1,7 +1,44 @@
 console.log('JS ok!');
 const nameElement = document.getElementById("name");
-console.log(nameElement);
+
 const travelElement = document.getElementById("travel");
-console.log(travelElement);
+
 const ageElement = document.getElementById("age");
-console.log(ageElement);
+
+// #Calcolare il prezzo per chilometro
+const ticketPrice = 0.21 * travelElement;
+let userDiscount;
+let finalPrice;
+// 3b. Applicare sconto 20% per i minorenni
+if (ageElement.value === 'Minorenne') {
+    userDiscount = (ticketPrice * 0.20).toFixed(2);
+
+    finalPrice = (ticketPrice - userDiscount).toFixed(2);
+
+    console.log('Il prezzo dopo aver applicato lo sconto è: ', finalPrice);
+
+
+} else if (ageElement.value === 'Maggiorenne') {
+    userDiscount = (ticketPrice * 0.40).toFixed(2);
+    console.log('Lo sconto per il passeggero è stato di: ', userDiscount);
+    finalPrice = (ticketPrice - userDiscount).toFixed(2);
+
+    console.log('Il prezzo dopo aver applicato lo sconto è: ', finalPrice);
+
+} else {
+    ageElement.value === 'Altro';
+    userDiscount = 0;
+    finalPrice = ticketPrice;
+    console.log('Lo sconto per il passeggero è stato di: 0');
+    console.log('Il prezzo del biglietto è: ', ticketPrice);
+}
+
+const generate = document.getElementById("generate");
+const cancel = document.getElementById("cancel");
+
+generate.addEventListener('click', function() {
+    const inputName = nameElement.value;
+    const inputTravel = travelElement.value;
+    const inuputAge = ageElement.value;
+    console.log(inputName, inputTravel, inuputAge);
+})
